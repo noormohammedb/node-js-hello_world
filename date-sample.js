1d849a5b3a8290b1b5b27b6edd659ddfee9f4fb3
noormohammedb <nooru.mohad@gmail.com>
2020-08-19 21:51:07 +0530

    try catch

---
+++ date-sample.js
@@ -0,0 +1,9 @@
+let t1 = new Date();
+console.log("Starting Time : " + t1.getMilliseconds());
+for(i = 0; i<99; i++)
+this;
+
+let t2 = new Date();
+console.log("Ending Time : " + t2.getMilliseconds());
+
+console.log(t2.getMilliseconds()-t1.getMilliseconds());
---
+++ try-catch.js
@@ -0,0 +1,10 @@
+try{
+    const vkl = 88;
+    // vkl = 33;
+    console.log("Success");
+}catch(err){
+    console.error(err);
+    console.log("Error ");
+}finally{
+    console.log("is that worked ? ");
+}
