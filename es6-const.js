a682e26b5184673a3f3f77e1641c27761a3ef12f
noormohammedb <nooru.mohad@gmail.com>
2020-08-20 11:53:48 +0530

    const array variable

--- es6-const.js
+++ es6-const.js
@@ -1,5 +1,20 @@
 const val = 200;
 
-val+=3;
+// val+=3;
 
 console.log(val);
+
+const arr = [1,2,3,4];
+
+console.log(arr);
+
+arr.push(5)
+
+console.log(arr);
+
+arr.shift();
+
+console.log(arr);
+
+// can't replace eniter array
+// arr = [6,7,8]
