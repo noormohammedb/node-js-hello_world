9a6c2a7adabdc4ee518737dfa15a67db08fff581
noormohammedb <nooru.mohad@gmail.com>
2020-08-20 12:32:53 +0530

    class inheritance

--- es6-class.js
+++ es6-class.js
@@ -1,5 +1,15 @@
-class hai{
+class hello{
+    constructor(){
+        console.log("Iam Hello constructor");
+    }
+    display(){
+        console.log("Hell hell");
+    }
+}
+
+class hai extends hello{
     constructor(val1=0,val2=0){
+        super();
         this.val1 = val1;
         this.val2 = val2;
     }
@@ -10,4 +20,5 @@ class hai{
 
 let obj = new hai(undefined,55);
 
+// function overloading 
 obj.display();
