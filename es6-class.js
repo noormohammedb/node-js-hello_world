37c31db370b5ab4eb1d9bd4f1d97ed84b3a6e8df
noormohammedb <nooru.mohad@gmail.com>
2020-08-20 12:13:36 +0530

    es6 class

---
+++ es6-class.js
@@ -0,0 +1,13 @@
+class hai{
+    constructor(val1=0,val2=0){
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+    display(){
+        console.log("Hello : "+(this.val1+this.val2));
+    }
+}
+
+let obj = new hai(undefined,55);
+
+obj.display();
