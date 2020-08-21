6b943c9ef73a33cb19cb400e321f6e8371304443
noormohammedb <nooru.mohad@gmail.com>
2020-08-21 12:24:14 +0530

    date optimized

--- date-sample.js
+++ date-sample.js
@@ -1,9 +1,6 @@
-let t1 = new Date();
-console.log("Starting Time : " + t1.getMilliseconds());
-for(i = 0; i<99; i++)
-this;
+let time = new Date();
+for(i = 0; i<99999; i++);
 
-let t2 = new Date();
-console.log("Ending Time : " + t2.getMilliseconds());
+let diff = new Date() - time;
 
-console.log(t2.getMilliseconds()-t1.getMilliseconds());
+console.log(diff);
